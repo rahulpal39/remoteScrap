3f78410ea45c7b4ea56c4147503e825bbded57a6
rahulpal39 <rahul@thinkdatalabs.com>
2022-11-23 10:04:41 +0530

    commita dd

--- scraper/scraper/spiders/productScrap.py
+++ scraper/scraper/spiders/productScrap.py
@@ -88,6 +88,8 @@ class QuotesSpider(scrapy.Spider):
         import json
 
     #     #header work post
+        pageData = response.css('.tab-section__main')
+
         JobData = response.css('div.body-section')
         prouctD = response.css("div.container")
         prouctFeature = response.css("span.i-list__text::text").extract()
@@ -101,36 +103,21 @@ class QuotesSpider(scrapy.Spider):
         overtitile = overview.css("h3.module-block__heading::text").extract()
         # overdetail=overview.css("h3.module-block__heading::text").extract()
         overperagraph = overview.css(
-            "p.module-block__paragraph::text").extract()
+            ".module-block__paragraph::text").extract()
         oversubhedding = overview.css(
-            "h4.module-block__subheading::text").extract()
-        print(oversubhedding)
-        print(overperagraph)
-        
+            ".module-block__subheading::text").extract()
+        # print(oversubhedding)
+        # print(overperagraph)
+        datas = {}
+        datas['introducation'] = overperagraph
 
-        
 #################################################specifications##################################
-        specifications = prouctD.css('div.side-section__main')
-        # specifTitle = specifications.css(
-        #     '.basic-accordion__heading::text').extract()
-        # specifsubH3 = specifications.css(
-        #     '.title-list__heading::text').extract()
-        # specifsubparaG = specifications.css(
-        #     'p.title-list__paragraph::text').extract()
-        # # print(overtitile, specifsubparaG)
-
-        # specifdat = specifications.css(
-        #     'div.basic-accordion__block').extract()
-        # print(json.dump(specifdat))
-        # json.dump(student, write_file, indent=4)
-        # res=log(specifsubH3,'demo')
+        specifications = prouctD.css('div.side-section__main')     
         spec = []
         dates_dict = dict()
-        num_list=[] 
-        # dict = dict() 
-
-        num_listparagrap=[]
-        num_listspTitls=[]
+        num_list = []
+        num_listparagrap = []
+        num_listspTitls = []
         property_data = {}
 
         for para in specifications.css('.basic-accordion__block'):
@@ -139,66 +126,145 @@ class QuotesSpider(scrapy.Spider):
             sptitile = para.css(".basic-accordion__heading::text").extract()
 
             # print(sptitile[0])
-            d=sptitile[0]
+            d = sptitile[0]
             # dict
             spec.append(sptitile[0])
-            
-          
 
-            for pa  in para.css('.title-list'):
+            for pa in para.css('.title-list'):
                 paragrap = pa.css(".title-list__paragraph::text").extract()
                 pTitls = pa.css(".title-list__heading::text").extract()
                 data = {}
                 num_listparagrap.append(paragrap)
                 num_listspTitls.append(pTitls)
-                dic=[]
+                dic = []
 
-                for pare in pTitls: 
-                    for itit in paragrap:       
-                        dic.append(itit) 
+                for pare in pTitls:
+                    for itit in paragrap:
+                        dic.append(itit)
                     for i in dic:
-                                        
-                        data[pare]=i
-                        property_data[d]=data
 
-                # print(pa.css(".title-list__heading::text").extract())
-                # print(pa.css(".title-list__paragraph::text").extract())
-                # spec.append(sptitile[0])
-                # # data={
-                # d[sptitile].append(pTitls)
-                # print(key) 
+                        data[pare] = i
+                        property_data[d] = data
+               
+        #################################resource###############################################
 
+        dataaa = pageData.css('.bold-title-heading::text').extract()
+        rheading = dataaa[0]
+        # filename = []
+        tableHeading = pageData.css(".border-table__th::text").extract()
+        filename = pageData.css(".js-checksum-filename::text").extract()
+        filetype = pageData.css(".js-checksum-type::text").extract()
+        filechecksum = pageData.css(
+            ".border-table__checksum__text::text").extract()
+        fileLink = pageData.css(".border-table__link")
+        apply_url = fileLink.css('a::attr(href)').extract()  # apply link grap
 
+        fileData = pageData.css(".border-table__dash-list")
+        osList = []
+        for fin in fileData:
+            fileLinks = fin.css(".border-table__list::text").extract()
+            osList.append(fileLinks)
+        datalunch = pageData.css(
+            '.date-short::text').extract()  # apply link grap
+
+        # print(osList)
+        # print(datalunch)
+        print(len(filename))
+        # print(filen)
+
+        # {
+        #     "NAME": "Firmware for VPort 06EC-2V Series",
+        #     "size": "43.4 MB",
+        #     "TYPE": "Firmware",
+        #     "Details":  {
+        #         "File Name": "Firmware for VPort 06EC-2V Series",
+        #         "Version": "v1.2",
+        #         "SHA-512 Checksum": "aa16fb972f0ed12ecaf78341509c4574c55aaf7d38b9f43f2dd89cca0418aa228b55c1ba4b03eb4dd732b21a9857a7083074a05a83e9ef61d3eede3a49eb48d6"
+        #     },
+        #     "VERSION": "v1.2",
+        #     "OPERATING SYSTEM": "-",
+        #     "RELEASE DATE": "Dec 03, 2021",
+        #     "Release notes": "https://cdn-cms.azureedge.net/Moxa/media/PDIM/S100000358/VPort%2006EC-2V%20Series_moxa-vport-06ec-2v-series-firmware-v1.2.rom_Software%20Release%20History.pdf"
+        # },
+       
+        filenam=filename
+        l = []
+
+        i = 0
+        import string
         
-            # print(property_data)
-        # tle=pTitle[0]
-        # pT=pTitle[0]
-        # phe=pheading[0]
-        # dictionary = {
-        #     'vendor': "MOXA",
-        #     'Product':tle ,
-        #     # 'Feature':,
-        #     'series': {
-        #         'Name': pT,
-        #         'Title': phe,
-        #         'Features and Benefits': prouctFeature,
-        #     },
-        #     'Overview': {
-        #         'overtitile':'demo',
+        while i < len(filename):
 
-        #         # overdetail: overperagraph,
-        #         # oversubhedding: overperagraph,
+            resdata={}  
+            resdata['NAME']=filename[i].replace("\r\n","") 
+            resdata['TYPE']=filetype[i].replace("\r\n","") 
+            resdata['Details']=filechecksum[i].replace("\r\n","") 
+            resdata['Release notes']=apply_url[i].replace("\r\n","") 
+            resdata['OPERATING SYSTEM']=osList[i]
+            resdata['RELEASE DATE']=datalunch[i].replace("\r\n","") 
 
-        #     },
-        #     "speacification":property_data,
+            # print(resdata)
+            # print(filename[i])
+            # print(apply_url[i])
+                # print(filechecksum[i])
+                # print(fileDatalist[i])
+                # print(osList[i])
+                # print(datalunch[i])
+            l.append(resdata)
+            i += 1
 
-          
+        # print(l)
+        # model-table"
 
-        # }
-        # # name=f"{tle}.json"
+        #############################Models#################################
+        # d=pageData.css('.model-table').extract():
+        models= pageData.css(".model-table")
+        model_Text = models.css('a::text').extract()#apply link grap
+        modelImage=models.css('.model-table__img::attr(src)').extract()
+        # modelImage = companyLogo.css('img').xpath('@src').extract()
+        modelTD=models.css('td::text').extract()
+        print(modelTD)
+        # print(model_url)
+
+        # reslist=[]
+            
+        # reslist['name']+=model_Text
+        # reslist['image']+=modelImage
+        # reslist['table']+=modelTD
+
+
+
+        # print(resdata)
+        # for a in pageData.css(".js-checksum-filename::text").extract():
+        #             # print(a)
+        #             tableHeading.append(a)
+        # border-table
+        #     # print(property_data)
+        # print(reslist)
+        tle=pTitle[0]
+        pT=pTitle[0]
+        phe=pheading[0]
+        dictionary = {
+            'vendor': "MOXA",
+            'Product':tle ,
+            # 'Feature':,
+            'series': {
+                'Name': pT,
+                'Title': phe,
+                'Features and Benefits': prouctFeature,
+            },
+            'Overview': {
+                'overtitile':datas,
+
+            },
+            "speacification":property_data,
+            'Resources':'reslist',
+
+        }
+        # name=f"{tle}.json"
         # # print(dictionary)
-        # # with open(name, "w") as outfile:
-        # #     json.dump(dictionary, outfile)
+        # with open(name, "w") as outfile:
+        #     json.dump(dictionary, outfile)
 
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
