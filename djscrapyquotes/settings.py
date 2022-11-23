4ee6622a4866a32422e8cdc182f27c8ae166d7c4
rahulpal39 <rahul@thinkdatalabs.com>
2022-11-23 16:02:30 +0530

    udpate comit

--- djscrapyquotes/settings.py
+++ djscrapyquotes/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = '/root/remoteScrap'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
