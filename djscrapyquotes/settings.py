05f9c9414b270b1c14a49bd4161ea53ec452526b
rahulpal39 <rahul@thinkdatalabs.com>
2022-12-16 14:51:17 +0530

    update commit

--- djscrapyquotes/settings.py
+++ djscrapyquotes/settings.py
@@ -80,18 +80,25 @@ WSGI_APPLICATION = 'djscrapyquotes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'remotejob',
+#         # 'USER': 'ubuntu',
+#         # 'PASSWORD': 'password@123',
+#         # 'HOST': 'localhost',
+#         # 'PORT': '',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'remotejob',
-        'USER': 'ubuntu',
-        'PASSWORD': 'password@123',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
